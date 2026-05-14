ce28c32294f197d311d282478ab9890e47c73ffd
taKana671 <kanae5321@gmail.com>
2026-05-14 11:25:57 +0900

    shapes/

--- scene.py
+++ scene.py
@@ -1,15 +1,16 @@
-import time
-from concurrent.futures import ProcessPoolExecutor
+from abc import ABC, abstractmethod
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 import numpy as np
 from panda3d.bullet import BulletRigidBodyNode
 from panda3d.bullet import BulletConvexHullShape, BulletPlaneShape
 from panda3d.core import NodePath
 from panda3d.core import Point3, Vec3, BitMask32, LColor
-from panda3d.core import AmbientLight, DirectionalLight
 
+from shapes import RandomConvexPolyhedron, ShatteredSphere
+from utils import clock
 from voronoi_generator.voronoi_3d.clip2cube import VoronoiClipped2Cube
-from shapes import RandomConvexPolyhedron
+from voronoi_generator.voronoi_3d.clip2sphere import VoronoiClipped2Sphere
 
 
 class VoronoiCell3D(NodePath):
@@ -41,7 +42,7 @@ class Ground(NodePath):
         self.set_pos(pos)
 
 
-class Scene:
+class Scene(ABC):
 
     def __init__(self):
         self.scene = NodePath('scene')
@@ -54,38 +55,21 @@ class Scene:
         self.cells = NodePath('cells')
         self.cells.reparent_to(self.scene)
 
-    def setup_light(self):
-        ambient_light = NodePath(AmbientLight('ambient_light'))
-        ambient_light.reparent_to(base.render)
-        ambient_light.node().set_color(LColor(0.6, 0.6, 0.6, 1.0))
-        base.render.set_light(ambient_light)
+    @abstractmethod
+    def clip(self, **kwargs):
+        """Clip to the specified shape."""
 
-        directional_light = NodePath(DirectionalLight('directional_light'))
-        directional_light.node().get_lens().set_film_size(200, 200)
-        directional_light.node().get_lens().set_near_far(1, 100)
-        directional_light.node().set_color(LColor(1, 1, 1, 1))
-        directional_light.set_pos_hpr(Point3(0, 0, 50), Vec3(-30, -45, 0))
-        # directional_light.node().show_frustom()
-        base.render.set_light(directional_light)
-        directional_light.node().set_shadow_caster(True)
-        base.render.set_shader_auto()
+    @abstractmethod
+    def clip_multiprocess(self, **kwargs):
+        """Clip to the specified shape in multiprocess."""
 
-    def create_voronoi_cube(self, config):
+    def create_voronoi_cells(self, config):
+        kwargs = {k: v for k, v in config.items() if k != 'multi_processing'}
+
         if config['multi_processing']:
-            self.clip_multoprocess(**config['clipping'], **config['polyhedron'])
+            self.clip_multiprocess(**kwargs)
         else:
-            self.clip(**config['clipping'], **config['polyhedron'])
-
-    def create_voronoi_cell(self, vertices, serial, cube_size, max_depth, scale):
-        model_creator = RandomConvexPolyhedron(vertices, max_depth, scale)
-        model = model_creator.create()
-
-        n = scale * cube_size / 2
-        pos = Point3(*model_creator.polyhedron_org_center * scale) - Vec3(n, n, 0)
-        color = LColor(*np.random.uniform(0, 1, 3), 1)
-        voronoi_cell = VoronoiCell3D(serial, model, pos, color)
-
-        return voronoi_cell
+            self.clip(**kwargs)
 
     def attach_voronoi_cell(self, voronoi_cell):
         # When changing a static body to a rigid body and applying a force, the `apply_central_force` only worked
@@ -95,35 +79,13 @@ class Scene:
         base.world.attach(voronoi_cell.node())
         voronoi_cell.node().set_mass(0)
 
-    def clip(self, cut_points, cube_size, diff, max_depth, scale):
-        """Clip voronoi cells to cube.
-            Args:
-                cut_points(int): the number of polyhedrons to divide a cube into.
-                cube_size (float): length of a cube's edge.
-                diff (float): how far from the vertices of the cube the dummy points should be placed.
-                max_depth (int): the number of divisions of one triangle; cannot be negative.
-                scale (float): the scale of the polyhedron; greater than 0.
-        """
-        start = time.perf_counter()
+    def create_voronoi_model(self, model_creator, serial, scale, offset):
+        model = model_creator.create()
+        pos = Point3(*model_creator.polyhedron_org_center * scale) + offset * scale
+        color = LColor(*np.random.uniform(0, 1, 3), 1)
+        voronoi_cell = VoronoiCell3D(serial, model, pos, color)
 
-        for serial, vertices in enumerate(VoronoiClipped2Cube(cut_points, cube_size, diff)):
-            voronoi_cell = self.create_voronoi_cell(vertices, serial, cube_size, max_depth, scale)
-            self.attach_voronoi_cell(voronoi_cell)
-
-        print(f'Took {time.perf_counter() - start}')
-
-    def clip_multoprocess(self, cut_points, cube_size, diff, max_depth, scale):
-        start = time.perf_counter()
-
-        with ProcessPoolExecutor() as executor:
-            results = [executor.submit(self.create_voronoi_cell, vertices, serial, cube_size, max_depth, scale)
-                       for serial, vertices in enumerate(VoronoiClipped2Cube(cut_points, cube_size, diff))]
-
-        for result in results:
-            voronoi_cell = result.result()
-            self.attach_voronoi_cell(voronoi_cell)
-
-        print(f'Took {time.perf_counter() - start}')
+        return voronoi_cell
 
     def set_mass_to_cells(self):
         for voroni_cell in self.cells.get_children():
@@ -133,3 +95,71 @@ class Scene:
     def apply_force(self):
         for voronoi_cell in self.cells.get_children():
             voronoi_cell.node().apply_central_force(Vec3.up() * 10)
+
+
+class ClippingMixin:
+
+    def get_future_result(self, futures):
+        for future in as_completed(futures):
+            voronoi_cell = future.result()
+            self.attach_voronoi_cell(voronoi_cell)
+
+
+class SphereClipping(ClippingMixin, Scene):
+
+    @clock()
+    def clip(self, cut_points, max_depth, scale):
+        """Clip voronoi cells to cube.
+            Args:
+                cut_points(int): the number of polyhedrons to divide a cube into.
+                max_depth (int): the number of divisions of one triangle; cannot be negative.
+                scale (float): the scale of the polyhedron; greater than 0.
+        """
+        for i, (polygons, spherical_idx) in enumerate(VoronoiClipped2Sphere(cut_points)):
+            voronoi_cell = self.create_voronoi_cell(i, polygons, spherical_idx, max_depth, scale)
+            self.attach_voronoi_cell(voronoi_cell)
+
+    @clock()
+    def clip_multiprocess(self, cut_points, max_depth, scale):
+        with ProcessPoolExecutor() as executor:
+            futures = [executor.submit(self.create_voronoi_cell, i, polygons, spherical_idx, max_depth, scale)
+                       for i, (polygons, spherical_idx) in enumerate(VoronoiClipped2Sphere(cut_points))]
+
+        self.get_future_result(futures)
+
+    def create_voronoi_cell(self, serial, polygons, spherical_idx, max_depth, scale):
+        model_creator = ShatteredSphere(polygons, spherical_idx, max_depth, scale)
+        voronoi_cell = self.create_voronoi_model(model_creator, serial, scale, Vec3(0, 0, 1))
+
+        return voronoi_cell
+
+
+class CubeClipping(ClippingMixin, Scene):
+
+    @clock()
+    def clip(self, cut_points, cube_size, diff, max_depth, scale):
+        """Clip voronoi cells to cube.
+            Args:
+                cut_points(int): the number of polyhedrons to divide a cube into.
+                cube_size (float): length of a cube's edge.
+                diff (float): how far from the vertices of the cube the dummy points should be placed.
+                max_depth (int): the number of divisions of one triangle; cannot be negative.
+                scale (float): the scale of the polyhedron; greater than 0.
+        """
+        for i, polygons in enumerate(VoronoiClipped2Cube(cut_points, cube_size, diff)):
+            voronoi_cell = self.create_voronoi_cell(i, polygons, max_depth, scale)
+            self.attach_voronoi_cell(voronoi_cell)
+
+    @clock()
+    def clip_multiprocess(self, cut_points, cube_size, diff, max_depth, scale):
+        with ProcessPoolExecutor() as executor:
+            futures = [executor.submit(self.create_voronoi_cell, i, polygons, max_depth, scale)
+                       for i, polygons in enumerate(VoronoiClipped2Cube(cut_points, cube_size, diff))]
+
+        self.get_future_result(futures)
+
+    def create_voronoi_cell(self, serial, polygons, max_depth, scale):
+        model_creator = RandomConvexPolyhedron(polygons, max_depth, scale)
+        voronoi_cell = self.create_voronoi_model(model_creator, serial, scale, Vec3(0))
+
+        return voronoi_cell
